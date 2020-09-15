c3ee8a1059af5a97c5509c6aa0a351c51aa6e2a8
Sandra RO <romeroortiz.sandra@gmail.com>
2020-09-15 23:02:50 +0200

    Update docs

---
+++ docs/precache-manifest.c6f2bdb5ab568c93fc522e3088dc5940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f25c38c43fde7fdb8736faa20a04244",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28ece558d29efd111c65",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10720d00cb6cf6c65bb1",
+    "url": "./static/js/2.720db37b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.720db37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ece558d29efd111c65",
+    "url": "./static/js/main.a7408903.chunk.js"
+  },
+  {
+    "revision": "169ad79fad7f9741cff3",
+    "url": "./static/js/runtime-main.13d094e3.js"
+  }
+]);
